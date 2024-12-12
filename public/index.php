31e6b808f0a5531bf327a913c6d1b1c9dd0ef933
Felype <felype.f.sposito@gmail.com>
2024-12-12 11:28:09 -0300

    Criação de variaveis e regras do PHP

--- public/index.php
+++ public/index.php
@@ -1,5 +1,29 @@
-<?php
+<!DOCTYPE html>
+<html>
+<body>
 
-echo 'xerequentooo';
+<?php
+$quebra = "<br>";
+$palavra = "PHP";
+$color = "vermelho";
+echo "Meu carro é " . $color . $quebra;
+echo "Eu estou aprendendo " . $palavra;
+$x = 7;
+$y = 9;
+echo $quebra . $quebra;
+echo "Conta do bala:";
+echo $quebra;
+echo $x*$y;
+echo $quebra . $quebra;
+
+var_dump($palavra);
+echo $quebra;
+var_dump($x);
+
+// Todas as variaveis recebem o valor de Fruta em String
+$a = $b = $c = "Fruta";
 
 ?>
+
+</body>
+</html>
